3efb7985886bb6f526094466475d8844dd0258fa
Ashot174 <ashot.grigoryan.90@inbox.ru>
2021-07-22 12:34:37 +0400

    Routing

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const path = require('path')
 const exphbs = require('express-handlebars')
+
+const homeRoutes = require('./routes/home')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
+
 
 const app = express()
 
@@ -14,33 +18,9 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static('public'))
-
-
-app.get('/', (req, res) => {
-    res.status(200)
-    //res.sendFile(path.join(__dirname, 'views', 'index.hbs'))
-    res.render('index', {
-        title: 'Main Page',
-        isHome: true
-    })
-})
-
-app.get('/courses', (req, res) => {
-    res.render('courses', {
-        title: 'Courses Page',
-        isCourses: true
-    })
-})
-
-app.get('/add', (req, res) => {
-    res.render('add', {
-        title: 'Add Course',
-        isAdd: true
-    })
-})
-
-
-
+app.use('/', homeRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/add', addRoutes)
 
 const PORT = process.env.PORT || 3000
 
---
+++ routes/courses.js
@@ -0,0 +1,12 @@
+const {Router} = require('express')
+const router = Router()
+
+router.get('/', (req, res) => {
+    res.render('courses', {
+        title: 'Courses Page',
+        isCourses: true
+    })
+})
+
+
+module.exports = router
